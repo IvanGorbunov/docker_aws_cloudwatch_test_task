9b405f777217726f5d77bf26337db1e5b432f686
IvanGorbunov <falseprogrammerfirst@gmail.com>
2024-05-01 08:33:30 +0300

    init

---
+++ main.py
@@ -0,0 +1,18 @@
+import asyncio
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+async def main(args):
+    pass
+
+
+def parse_args():
+    pass
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    asyncio.run(main(args))
